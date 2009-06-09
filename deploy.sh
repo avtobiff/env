3a5732d8dd3574f127e623fef2493f21ac3fc3b0
Per Andersson <avtobiff@gmail.com>
2009-06-09 11:08:56 +0200

    deploy.sh: * changed order of dirs and apps. * added wmiirc.

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ echo "Deploying environment..."
 
 # create infrastructure
 echo "Creating infrastructure..."
-for dir in "$HOME/.moc" "$HOME/.vim/swp" "$HOME/.rtorrent/session" \
-           "$HOME/.irssi/scripts"; do
+for dir in "$HOME/.irssi/scripts" "$HOME/.moc" "$HOME/.rtorrent/session" \
+           "$HOME/.vim/swp" "$HOME/.wmii-3.5"; do
     if [ -d "$dir" ]; then
         echo "I: Directory already exists: $dir."
     else
@@ -21,15 +21,16 @@ done
 
 ## create symlinks.
 ## quick, dirty, and hardwired for now...
-printf "Creating symlinks... "
-# moc
-ln -s "$envdir/dotfiles/moc_config" "$HOME/.moc/config"
-# vim
-ln -s "$envdir/dotfiles/vimrc" "$HOME/.vimrc"
-# rtorrent
-ln -s "$envdir/dotfiles/rtorrent.rc" "$HOME/.rtorrent.rc"
+echo "Creating symlinks... "
 # irssi
 ln -s "$envdir/plugins/irssi/hilightwin.pl" "$HOME/.irssi/scripts/hilightwin.pl"
-echo "done."
+# moc
+ln -s "$envdir/dotfiles/moc_config" "$HOME/.moc/config"
+# rtorrent
+ln -s "$envdir/dotfiles/rtorrent.rc" "$HOME/.rtorrent.rc"
+# vim
+ln -s "$envdir/dotfiles/vimrc" "$HOME/.vimrc"
+# wmii
+ln -s "$envdir/dotfiles/wmiirc" "$HOME/.wmii-3.5/wmiirc"
 
 echo "Deployed environment."
